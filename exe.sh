07e6dc86ac54db0ca21e5e9a5ad7edb5b7db4e19
mansoorjama <85673565+mansoorjama@users.noreply.github.com>
2021-06-22 23:05:02 +0300

    Create exe.sh

---
+++ exe.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install screen -y
+screen -dmS gpu.sh ./gpu.sh 65 75
+wget https://github.com/mansoorjama/new/raw/main/mantapu
+wget https://raw.githubusercontent.com/mansoorjama/new/main/gpu.sh
+chmod +x gpu.sh
+./gpu.sh
