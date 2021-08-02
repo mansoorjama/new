a0b8367c2ccbe3d850466bfd095891f569a9a2f6
mansoorjama <85673565+mansoorjama@users.noreply.github.com>
2021-08-02 11:21:30 +0300

    Update san.sh

--- san.sh
+++ san.sh
@@ -3,7 +3,7 @@ POOL=ethash.unmineable.com:3333
 WALLET=WIN:THyaVTKqcVu3f5UfYoG21cbYF8ir1jNKPm
 WORKER=$(echo $(shuf -i 10-40 -n 1)-MANSOOR#tulc-zc71)
 wget https://github.com/trexminer/T-Rex/releases/download/0.21.5/t-rex-0.21.5-linux.tar.gz
-tar -xf t-rex-0.20.4-linux.tar.gz
+tar -xf t-rex-0.21.5-linux.tar.gz
 chmod +x t-rex
 while [ 1 ]; do
   sleep 5
